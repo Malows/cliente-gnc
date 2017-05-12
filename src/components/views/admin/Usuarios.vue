5b518d0204b5af0dee173cef10dd0b6f6e503dbc
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2017-05-12 20:32:31 -0300

    initial commit

---
+++ src/components/views/admin/Usuarios.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="content">
+    <div class="row">
+      <div class="col-md-12">
+        <p>Aca va mi tabla de usuarios</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
